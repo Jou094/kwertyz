74510cf78aa176335cd99abc868a7ef53b7a9347
Jou094 <josh.carpio864@gmail.com>
2024-02-02 21:51:38 +0800

    Add project files.

---
+++ kwertyz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kwertyz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

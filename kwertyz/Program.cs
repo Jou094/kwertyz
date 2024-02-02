c45c07a337d33faf190b04008d715ddc0ee175be
uwany <151894847+Yuanjose12@users.noreply.github.com>
2024-02-02 22:15:19 +0800

    wawawasd

--- kwertyz/Program.cs
+++ kwertyz/Program.cs
@@ -6,7 +6,7 @@ namespace kwertyz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Carps!");
+            Console.WriteLine("Hello Carps! Nigrows");
         }
     }
 }
